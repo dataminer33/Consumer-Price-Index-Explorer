13f1796ef9fb655e6cc8ab83628920f0d07c3873
dataminer33 <alexandre.f.c.dasilva@gmail.com>
2024-09-27 12:14:59 +0200

    updated wegapp

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Consumer-Price-Index-Explorer
-"CPI Explorer" is a data-driven project focused on analyzing and comparing Consumer Price Index trends across various regions and time periods.  The project showcases the use of Python, pandas, and Plotly for interactive data exploration, as well as Streamlit for building a user-friendly web interface.
+# Global Consumer Price Index Explorer
+
+![Project Preview](static/images/preview.png)
+
+## Overview
+
+The **Global Consumer Price Index Explorer** is an interactive web application that provides insights into the global Consumer Price Index (CPI) trends using data visualizations. Built with [Streamlit](https://streamlit.io/) and [Plotly](https://plotly.com/), the app allows users to explore and visualize CPI data from various countries over time.
+
+## Features
+
+- **Data Visualization**: Interactive plots using Plotly to analyze Consumer Price Index trends globally.
+- **Custom Background**: An aesthetically pleasing interface with a custom background image.
+- **Efficient Data Caching**: Uses Streamlit's caching mechanism to efficiently load data.
+
+## How to Use
+
+1. **Access the App**: Visit the [![Global CPI Explorer Web App](https://img.shields.io/badge/Go_to_App-Web_App-brightgreen)](https://swiss-rent-and-salary-trends.streamlit.app/)
+
+2. **Interact with the App**: 
+   - Explore Consumer Price Index data using interactive charts.
+   - Customize the view by selecting specific countries or date ranges.
+   - The app provides real-time visual feedback on the selected data.
+
+## Tech Stack
+
+- **Streamlit**: For building the web application.
+- **Pandas**: For data manipulation and analysis.
+- **Plotly**: For creating interactive visualizations.
+- **Python**: The core programming language for development.
+"""
--- streamlip_app.py
+++ streamlip_app.py
@@ -4,28 +4,16 @@ import plotly.express as px
 import base64, os
 
 
-
-color_scheme = {
-    'primary': '#1e3d59',    # Dark blue
-    'secondary': '#f5f0e1',  # Off-white
-    'accent1': '#ff6e40',    # Orange
-    'accent2': '#ffc13b',    # Yellow
-    'text': '#ffffff',       # White
-    'background': 'rgba(30, 61, 89, 0.8)'  # Semi-transparent dark blue
-}
-
 # Load data
 @st.cache_data
 def load_data():
-    # Placeholder for the actual data loading code
-    # Replace this with the code to load your CPI data
-    data = pd.read_csv('data/processed/data_v1.csv')  # Adjust this line to your actual data source
+    data = pd.read_csv('data/processed/data_v1.csv')
     return data
 
-# App Title
-st.markdown('<div class="header-style"><h1>Global CPI Analysis: Food and General Consumer Price Index</h1></div>', unsafe_allow_html=True)
 
 
+# Set page configuration
+st.set_page_config(page_title="Global CPI Explorer", layout="wide")
 
 # Function to encode the image
 def get_base64_of_bin_file(bin_file):
@@ -34,34 +22,15 @@ def get_base64_of_bin_file(bin_file):
     return base64.b64encode(data).decode()
 
 
-background_image_path = get_base64_of_bin_file('static/images/background_2.png')
-
+background_image = get_base64_of_bin_file('static/images/background_2.png')
 
    
 page_bg_img = f'''
 <style>
-[data-testid="stAppViewContainer"] > .main {{
-    background-image: url("data:image/png;base64,{background_image_path}");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-size: cover;
-}}
-.stApp {{
-        background-color: {color_scheme['background']};
-        color: {color_scheme['text']};
-}}
-.stButton>button {{
-    color: {color_scheme['primary']};
-    background-color: {color_scheme['secondary']};
-}}
-.stSidebar>div {{
-    color: {color_scheme['primary']};
-}}
-.stTab {{
-    background-color: {color_scheme['secondary']};
-    color: {color_scheme['primary']};
-}}
+ .stApp {{
+        background-image: url("data:image/png;base64,{background_image}");
+        background-size: cover;
+    }}
 .stApp header {{
         background-color: rgba(0,0,0,0);
 }}
@@ -76,6 +45,10 @@ page_bg_img = f'''
 }}
 </style>
 ''' 
+
+# App Title
+st.markdown('<div class="header-style"><h1>Global Consumer Price Index Explorer</h1></div>', unsafe_allow_html=True)
+
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
@@ -110,19 +83,24 @@ filtered_data = filtered_data[filtered_data[region_type].isin(selected_region)]
 
 # CPI Comparison Plot
 st.markdown(f'<div class="header-style"><h2>CPI Comparison ({region_type})</h2></div>', unsafe_allow_html=True)
-cpi_type = st.selectbox('Select CPI Type:', ['Consumer Prices, Food Indices (2015 = 100)', 'Consumer Prices, General Indices (2015 = 100)', 'Food price inflation'])
+cpi_type = st.selectbox('Select CPI Type:', ['Consumer Prices, Food Indices (2015 = 100)', 'Consumer Prices, General Indices (2015 = 100)'])
 filtered_data = filtered_data[filtered_data['Item'] == cpi_type]
 treemap_data = filtered_data.groupby(['Continent', 'Country', 'Year'])['Value'].mean().reset_index()
 filtered_data = filtered_data.groupby([region_type, 'Year'])['Value'].mean().reset_index()
 
-fig = px.line(filtered_data, x='Year', y='Value', color=region_type, height=600, width=1200)
+fig = px.line(filtered_data, x='Year', y='Value', 
+              color=region_type, 
+              height=600, 
+              width=1200)
+
 fig.update_xaxes(tickfont=dict(color='white', size=16))
 fig.update_yaxes(tickfont=dict(color='white', size=14))
+
 fig.update_layout(
     title=dict(text=f'{cpi_type} Trends', font=dict(size=20)),
     xaxis_tickangle=45
 )
-st.plotly_chart(fig)
+st.plotly_chart(fig,use_container_width=True)
 
 # Identify largest increases and decreases
 st.markdown(f'<div class="header-style"><h2>Largest CPI Changes from {start_year} to {end_year}</h2></div>', unsafe_allow_html=True)
@@ -136,12 +114,14 @@ if region_type == 'Country':
     # Bar chart for country-level changes
     cpi_changes_sorted = cpi_changes.sort_values('Change in %', ascending=False)
     fig_bar = px.bar(cpi_changes_sorted, x=region_type, y='Change in %', 
-                     color='Change in %', height=600, width=1200,
+                     color='Change in %',
                      color_continuous_scale=px.colors.sequential.Viridis,
                      hover_data={region_type: True, 'Change in %': ':.2f'})
+    
     fig_bar.update_layout(title=f'CPI Changes by Country ({start_year} to {end_year})',
                           xaxis_title='Country', yaxis_title='Change in %')
-    st.plotly_chart(fig_bar)
+    
+    st.plotly_chart(fig_bar, use_container_width=True)
 else:
     
     # Calculate mean values for each country
@@ -149,26 +129,20 @@ else:
     
     # Sort countries within each continent by mean value
     country_means = country_means.sort_values(['Continent', 'Value'], ascending=[True, False])
-    
-    # Calculate continent totals (sum of country means)
-    #continent_totals = country_means.groupby('Continent')['Value'].sum().reset_index()
-    #continent_totals['Country'] = continent_totals['Continent']  # This ensures continents appear in the hierarchy
-    
-    # Combine country and continent data
-    # treemap_data = pd.concat([country_means, continent_totals])
-    
+
     # Create the treemap
     fig_treemap = px.treemap(country_means, 
                              path=[px.Constant("World"),'Continent', 'Country'], 
                              values='Value', 
-                             color='Value', 
-                             height=600, 
-                             width=1200,
-                             color_continuous_scale=px.colors.sequential.Viridis
+                             color='Value',
+                             color_continuous_scale=px.colors.sequential.Viridis,
+                             height=600
                              )
+    
     fig_treemap.update_traces(hovertemplate='%{label}<br>Value: %{value:.2f}')
     fig_treemap.update_layout(title=f'Mean CPI Values by Continent and Country ({start_year} to {end_year})')
-    st.plotly_chart(fig_treemap)
+    
+    st.plotly_chart(fig_treemap, use_container_width=True)
 
 # Display the regions with the biggest rises
 largest_rise = cpi_changes.nlargest(10, 'Change in %')
